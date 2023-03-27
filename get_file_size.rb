0cbb5cbaa97b336222c7883fa69bb56aa230fd98
linlin <1208215066@qq.com>
2023-03-27 09:24:09 +0800

    svg生成的脚本

---
+++ get_file_size.rb
@@ -0,0 +1,16 @@
+class Integer
+  def to_filesize
+    {
+      'B'  => 1024,
+      'KB' => 1024 * 1024,
+      'MB' => 1024 * 1024 * 1024,
+      'GB' => 1024 * 1024 * 1024 * 1024,
+      'TB' => 1024 * 1024 * 1024 * 1024 * 1024
+    }.each_pair { |e, s| return "#{(self.to_f / (s / 1024)).round(2)}#{e}" if self < s }
+  end
+end
+
+puts File.size('hi.html')
+a = File.size('hi.html')
+puts a.to_filesize
+number_to_human_size(a)
